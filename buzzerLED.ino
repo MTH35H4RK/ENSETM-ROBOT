8a3345ccaf185ab7fe5e18c45a01fb7c8cc16596
TH3_5H4RK <34583547+MTH35H4RK@users.noreply.github.com>
2023-05-14 03:56:04 +0100

    added LED function

--- buzzerLED.ino
+++ buzzerLED.ino
@@ -1,9 +1,14 @@
 int buzzerPin = 8;
+int redLedPin = 9;
+int blueLedPin = 10;
+
 int sirenState = 0;
 unsigned long lastSirenChangeTime = 0;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
 }
 
 void loop() {
@@ -17,10 +22,12 @@ void loop() {
     // Play tone based on siren state
     if (sirenState == 1) {
       tone(buzzerPin, 880);
+      digitalWrite(redLedPin, HIGH);
+      digitalWrite(blueLedPin, LOW);
     } else {
       tone(buzzerPin, 660);
+      digitalWrite(redLedPin, LOW);
+      digitalWrite(blueLedPin, HIGH);
     }
   }
-  
-  //CODE GLOBAL AYKON HNA
 }
